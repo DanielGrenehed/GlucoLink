f989fcb5b39d5f16354d736808f0de8200c86f45
DanielGrenehed <daniel.grenehed97@gmail.com>
2023-10-23 00:10:37 +0200

    python refactor

--- LibreLinkUpTest.py
+++ LibreLinkUpTest.py
@@ -2,6 +2,11 @@
 # based on https://gist.github.com/khskekec/6c13ba01b10d3018d816706a32ae8ab2
 import requests
 
+def save_file(filename, data):
+    f = open(filename, "w")
+    f.write(str(data))
+    f.close()
+
 url_base='https://api-eu.libreview.io'
 headers={'accept-encoding':'gzip',
          'cache-control':'no-cache',
@@ -23,40 +28,34 @@ def gmc_data(pid):
     endpoint = url_base + "/llu/connections/" + str(pid) + "/graph"
     return requests.get(endpoint, headers=headers)
 
+def handle_response(response, tag):
+    if (response.status_code != 200):
+        print(f"{tag} failed: (status {response.status_code})\nExiting...")
+        exit()
+    if (response.json()['status'] != 0):
+        print(f"{tag} failed: {response.json()['error']['message']}\nExiting...")
+        exit()
 
 # login
 credentials = {"email": str(input("email: ")), "password":str(input("password: "))}
 auth_req = login(credentials)
-if (auth_req.status_code != 200):
-    print(f"Failed to login: (status {req.status_code})\nExiting...")
-    exit()
-if (auth_req.json()['status'] != 0):
-    print(f"login failed: {req.json()['error']['message']}\nExiting...")
-    exit()
+handle_response(auth_req, "login")
 print(f"\nLogin: {auth_req}")
 headers['Authorization'] = f"Bearer {str(auth_req.json()['data']['authTicket']['token'])}"
-
+save_file("login.json", auth_req.json())
 
 # get connections
 con_req = connections()
-if (con_req.status_code != 200):
-    print(f"Failed to get connections: (status {con_req.status_code})\nExiting...")
-    exit()
-if (con_req.json()['status'] != 0):
-    print(f"Failed to get connections: {con_req.json()['error']['message']}\nExiting...")
-    exit()
+handle_response(con_req, "Get connections")
 print(f"\nConnections: {con_req}")
 print(con_req.json()['data'][0])
 pid = con_req.json()['data'][0]['patientId']
+save_file("connections.json", con_req.json())
 
 
 # get gmc data
 gmc_req = gmc_data(pid)
-if (gmc_req.status_code != 200):
-    print("Failed to read gmc data: (status {gmc_req.status_code})\nExiting...")
-    exit()
-if (gmc_req.json()['status'] != 0):
-    print(f"GMC retrieval failed: {gmc_req.json()['error']['message']}\nExiting...")
-    exit()
+handle_response(gmc_req, "Get GMC data")
 print(f"\nGMC data: {gmc_req}")
 print(gmc_req.json())
+save_file("gmc.json", gmc_req.json())
